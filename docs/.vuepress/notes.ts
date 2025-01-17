abcb274c798e389dc1d57dcb8c03e4d2f8bbc3e9
yybigfish <130434023+yybigfish233333@users.noreply.github.com>
2025-01-17 22:07:17 +0800

    Update notes.ts

--- docs/.vuepress/notes.ts
+++ docs/.vuepress/notes.ts
@@ -3,12 +3,12 @@ import { defineNoteConfig, defineNotesConfig } from 'vuepress-theme-plume'
 const demoNote = defineNoteConfig({
   dir: 'demo',
   link: '/demo',
-  sidebar: ['', 'foo', 'bar'],
+  sidebar: ['', 'bar'],
 })
 
 const extendhashtable=defineNoteConfig({
   dir: 'Extendible_Hash_Table',
-  link: '/Extendible_Hash_Table/',
+  link: '/Extendible_Hash_Table',
   sidebar: ['', '/基于可拓展哈希表的索引结构的实现.md'],
 })
 
